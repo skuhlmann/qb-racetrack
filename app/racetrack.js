9daaa7f20144a82a45c56279a58ac32b5a5d4ea3
skuhlmann <samkuhlmann@gmail.com>
2015-05-21 14:16:21 -0600

    QBSync Auto Commit of racetrack.js

--- app/racetrack.js
+++ app/racetrack.js
@@ -1,14 +1,31 @@
+    //*************************************************\\
+    //================ DOCUMENT.READY =================\\
+    //*************************************************\\
+
+$(document).ready(function(){
+
+
+});
+
 var config = {
   races: {
     dbid: "bjxj6mk28",
     rid: 3,
     name: 6,
     purse: 7,
-    type: 8,
     distance: 9,
     date: 15
   }
 };
 
 var client = new Base({config: config});
+
+function showRace(){
+  var races = client.getRecordInfo("races", 1);
+  $("#race-list").toggleClass("hidden");
+};
+
+
+
+
 
