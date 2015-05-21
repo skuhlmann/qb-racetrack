034b034b6422e6959b436e0f4f905da07113f80f
skuhlmann <samkuhlmann@gmail.com>
2015-05-21 15:01:58 -0600

    QBSync Auto Commit of racetrack.js

--- app/racetrack.js
+++ app/racetrack.js
@@ -17,7 +17,7 @@ var client = new Base({config: config});
 //functions
 
 function showRaces(){
-  console.log("races");
+  console.log(races);
   var races = client.getRecordInfo("races", 1)
   $("#race-list").removeClass("hidden");
 };
