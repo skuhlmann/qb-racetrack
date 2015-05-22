425514cd5c2d22657911bbccaedb18494a104d0e
skuhlmann <samkuhlmann@gmail.com>
2015-05-22 11:43:15 -0600

    QBSync Auto Commit of racetrack.js

--- app/racetrack.js
+++ app/racetrack.js
@@ -24,7 +24,7 @@ var Race = {
 
     for (var i in races) {
       var race = races[i];
-      var $listTemplate = app.$raceList.clone(true)
+      var $listTemplate = app.$raceList.clone(true).show();
 
       $listTemplate.find('.raceName').text(race.name);
       $listTemplate.find('.racePurse').text("Purse: $ " + race.purse);
