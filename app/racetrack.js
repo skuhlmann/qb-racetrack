6b20335ad896014566aef624d7574739b3a91ad3
skuhlmann <samkuhlmann@gmail.com>
2015-05-22 11:59:26 -0600

    QBSync Auto Commit of racetrack.js

--- app/racetrack.js
+++ app/racetrack.js
@@ -29,7 +29,7 @@ var Race = {
       $listTemplate.find('.raceName').text(race.name);
       $listTemplate.find('.raceDate').text("Date: " + BaseHelpers.dateToString(race.date));
       $listTemplate.find('.racePurse').text("Purse: $ " + race.purse);
-      $listTemplate.find('.raceDistance').text("Distance: " + race.distance + "furlongs");
+      $listTemplate.find('.raceDistance').text("Distance: " + race.distance + " furlongs");
 
       section.push($listTemplate)
     }
